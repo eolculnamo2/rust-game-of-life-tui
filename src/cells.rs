14d58c82e384a11693ca5a8b576a6f042f0d129b
Rob Bertram <rbertram8@gmail.com>
2022-11-28 01:31:40 -0500

    init commit

---
+++ src/cells.rs
@@ -0,0 +1,93 @@
+use rand::Rng;
+
+pub const BOARD_WIDTH: i16 = 50;
+pub const BOARD_HEIGHT: i16 = 50;
+pub const BOARD_AREA: i16 = BOARD_WIDTH * BOARD_HEIGHT;
+
+
+#[derive(Clone)]
+pub struct Cell {
+    pub is_alive: bool,
+    pub id: i16,
+}
+
+fn rand_bool() -> bool {
+    let mut rng = rand::thread_rng();
+    rng.gen_bool(0.5)
+}
+
+pub fn init_vec() -> Vec<Cell> {
+    let mut cell_list = vec![];
+    for i in 0..=BOARD_AREA {
+        let c = Cell {
+            is_alive: rand_bool(),
+            id: i,
+        };
+        cell_list.push(c);
+    }
+    cell_list
+}
+
+// can do more with this later(maybe/probably wont)
+fn is_cell_neighbor(i: i16) -> bool {
+    i >= 0 && i <= BOARD_AREA
+}
+
+fn get_neighbor_indexes(cell_id: i16) -> Vec<i16> {
+    let neighbors: Vec<i16> = vec![
+        // TOP
+        cell_id - BOARD_WIDTH - 1,
+        cell_id - BOARD_WIDTH,
+        cell_id - BOARD_WIDTH + 1,
+        // SIDES
+        cell_id + 1,
+        cell_id - 1,
+        // BOTTOM
+        cell_id + BOARD_WIDTH - 1,
+        cell_id + BOARD_WIDTH,
+        cell_id + BOARD_WIDTH + 1,
+    ];
+
+    neighbors
+        .into_iter()
+        .filter(|i| is_cell_neighbor(*i))
+        .collect::<Vec<i16>>()
+}
+
+fn is_cell_alive(living: bool, neighbor_cells: Vec<Cell>) -> bool {
+    let live_count =
+        neighbor_cells.into_iter().fold(
+            0,
+            |total, current| if current.is_alive { total + 1 } else { total },
+        );
+    match (living, live_count) {
+        (true, 2 | 3) => true,
+        (false, 3) => true,
+        _ => false,
+    }
+}
+
+pub fn handle_generation_change(curr_cells: Vec<Cell>) -> Vec<Cell> {
+    curr_cells
+        .clone()
+        .into_iter()
+        .map(|cell| {
+            // todo couldn;t get map to work but should try again now that Ive added Clone to Cell
+            let mut neighbors: Vec<Cell> = vec![];
+            let neighbors_ids: Vec<i16> = get_neighbor_indexes(cell.id);
+            for i in 0..neighbors_ids.len() {
+                let n_id = neighbors_ids.get(i).unwrap();
+                if let Some(n) = curr_cells.get(*n_id as usize) {
+                    neighbors.push(n.clone());
+                } else {
+                    println!("no cell found at {}", *n_id);
+                }
+            }
+            // todo couldnt get spread to work here for id
+            Cell {
+                is_alive: is_cell_alive(cell.is_alive, neighbors),
+                id: cell.id,
+            }
+        })
+        .collect::<Vec<Cell>>()
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use array_macro::array;
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::io::Stdout;
+use std::sync::mpsc;
+use std::time::{Duration, Instant};
+use std::{io, thread};
+use tui::backend::CrosstermBackend;
+use tui::layout::Constraint;
+use tui::style::{Color, Style};
+use tui::widgets::{Row, Table, Cell};
+use tui::Terminal;
+
+mod cells;
+
+fn tear_down(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), io::Error> {
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
+
+fn map_cells_to_rows(cells: Vec<cells::Cell>) -> Vec<Row<'static>> {
+    let mut cell_rows: Vec<Row> = vec![];
+    let mut current_row: Vec<Cell> = vec![];
+    for i in 0..cells.len() {
+        let cell = cells.get(i).unwrap();
+        // current_row.push(cell.id.to_string());
+        current_row.push(match cell.is_alive {
+            true => Cell::from("").style(Style::default().bg(Color::LightGreen)),
+            false => Cell::from("").style(Style::default().bg(Color::DarkGray)),
+        });
+        if cell.id != 0 && cell.id % cells::BOARD_WIDTH == 0  {
+            let new_row = Row::new(current_row);
+            cell_rows.push(new_row);
+            current_row = vec![];
+        }
+    }
+    cell_rows
+}
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut cell_list = cells::init_vec();
+
+    let max_iter = 200;
+    let mut cur_iter = 0;
+    let widths = array![Constraint::Length(5); cells::BOARD_WIDTH as usize];
+
+    while cur_iter < max_iter {
+        cell_list = cells::handle_generation_change(cell_list);
+        terminal.draw(|f| {
+            let rows = map_cells_to_rows(cell_list.clone());
+            let table_block = Table::new(rows)
+                .style(Style::default().fg(Color::White))
+                .widths(&widths);
+            f.render_widget(table_block, f.size());
+        })?;
+        thread::sleep(Duration::from_millis(50));
+        cur_iter += 1;
+    }
+    tear_down(&mut terminal)?;
+    Ok(())
+}
